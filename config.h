3198001a1a76f078d761736f0330732445820a7b
shivjeet1 <shivamlavhalepatil@gmail.com>
2024-12-12 11:20:44 +0000

    font increase

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 7;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=11:antialias=true:autohint=true", "Symbols Nerd Font Mono:style=Regular:size=11:antialias=true:autohint=true", "Noto Color Emoji:style=Regular:pixelsize=12:antialias=true:autohint=true" };
-static const char dmenufont[]       = "JetBrains Mono:size=11";
+static const char *fonts[]          = { "JetBrains Mono:size=16:antialias=true:autohint=true", "Symbols Nerd Font Mono:style=Regular:size=11:antialias=true:autohint=true", "Noto Color Emoji:style=Regular:pixelsize=12:antialias=true:autohint=true" };
+static const char dmenufont[]       = "JetBrains Mono:size=16";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 
