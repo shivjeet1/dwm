6c1f7f74c09cd0d573cde113ae6f52e7c9e0b808
shivjeet1 <shivamlavhalepatil@gmail.com>
2024-12-20 16:29:43 +0530

    -veworkspaces

--- config.h
+++ config.h
@@ -106,10 +106,6 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
